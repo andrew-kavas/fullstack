a8b73436cbb5c42d3de6b1282eeff5ae0b01129c
Andrew Kavas <ask410ventures@gmail.com>
2024-06-21 18:52:57 -0500

    structure

--- fullstack-app/src/App.js
+++ fullstack-app/src/App.js
@@ -1,25 +1,7 @@
-import { useEffect, useState } from 'react';
+import Main from '#src/components/main.js';
 
-import config from '#src/config.js';
-
-const { console, fetch } = globalThis;
-
-function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:4000/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
-  }, []);
-
-  console.log(config);
-
-  return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
-      <h1 className='text-4xl text-blue-500'>{message}</h1>
-    </div>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
 export default App;
