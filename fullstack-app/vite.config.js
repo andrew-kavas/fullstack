def9676cb464fc9e8a2abd5995ee1bfdd0a006e4
Andrew Kavas <ask410ventures@gmail.com>
2024-06-24 15:43:00 -0500

    wip switch to vite

---
+++ fullstack-app/vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'build'
+  }
+});
