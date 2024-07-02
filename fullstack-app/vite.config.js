d0bc760e6b950a7e6a5f1a56f70e8bbb45655831
Andrew Kavas <ask410ventures@gmail.com>
2024-07-02 18:25:57 -0500

    remove nginx, simplify app

--- fullstack-app/vite.config.js
+++ fullstack-app/vite.config.js
@@ -1,13 +1,36 @@
+// frontend/vite.config.js
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 3000
+    host: '0.0.0.0',
+    port: 3000, // Ensure this is consistent with your Docker Compose setup
+    strictPort: true // Ensures Vite will only start if the port is available
   },
-  build: {
-    outDir: 'build'
-  }
+  plugins: [react()]
 });
+
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000
+//   },
+//   // build: {
+//   //   outDir: 'build'
+//   // },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         format: 'es',
+//         strict: false,
+//         // entryFileNames: '[name].js',
+//         dir: 'dist/'
+//       }
+//     }
+//   }
+// });
--- tmp/fullstack-app/src/App.js
+++
@@ -1,7 +0,0 @@
-import Main from '#src/components/main.js';
-
-const App = () => {
-  return <Main />;
-};
-
-export default App;
