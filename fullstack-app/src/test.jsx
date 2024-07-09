9c81dfa7b5e8d7ba666d66d22ee9f3b9edbaeb7a
Andrew Kavas <ask410ventures@gmail.com>
2024-07-09 17:45:11 -0500

    simplify

--- fullstack-app/src/test.jsx
+++ fullstack-app/src/test.jsx
@@ -1,32 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
-
 import Boundary from '#src/components/boundary.jsx';
 import Clickable from '#src/components/clickable.jsx';
+import LoadingArea from '#src/components/loading-area.jsx';
 import useAsync from '#src/hooks/use-async.js';
 
 const { console, fetch } = globalThis;
 
 const Test = () => {
-  // export default () => {
-  // const [displayData, setDisplayData] = useState();
-  // const isFetching = useRef(false); // Use a ref to track if the data is being fetched
-
-  // useEffect(() => {
-  //   if (data || isFetching.current) return;
-  //   isFetching.current = true; // Mark as fetching
-  //   getData();
-  // }, [data]);
-
-  // async function getData() {
-  //   const response = await fetch('http://localhost:4000/api/hello');
-  //   const data = await response.text();
-  //   console.log(data);
-  //   setDisplayData(data);
-  //   isFetching.current = false; // Reset the fetching status
-  // }
   const { data, error, execute, isLoading } = useAsync(async () => {
-    // const response = await fetch('http://localhost:4000/api/hello');
-    const response = await fetch('http://localhost:4000/api/error');
+    // switch to the commented line to test the error handling
+    const response = await fetch('http://localhost:4000/api/hello');
+    // const response = await fetch('http://localhost:4000/api/error');
     console.log(response);
 
     if (!response.ok) {
@@ -39,17 +22,13 @@ const Test = () => {
   });
   console.log({ error });
 
-  if (error) return <div className='text-red-500'>{error.message}</div>;
-
   return (
     <Boundary>
-      <div>Test</div>
-      {/* {error && <div className='text-red-500'>Error: {error}</div>} */}
-      {isLoading && <div>Loading...</div>}
-      <div className='text-red-500'>Test</div>
-      <Clickable onClick={execute}>Fetch Data</Clickable>
       <div className='text-orange-500'>Testing</div>
-      {data && <>{data}</>}
+      <Clickable onClick={execute}>Fetch Data</Clickable>
+      {isLoading && <LoadingArea />}
+      {error && <div className='text-red-500'>Error: {error.message}</div>}
+      {data && <div className='text-green-500'>{data}</div>}
     </Boundary>
   );
 };
