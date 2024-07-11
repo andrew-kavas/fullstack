29308385726b37ba5e0630dfdedb304b9564da4e
Andrew Kavas <ask410ventures@gmail.com>
2024-07-10 22:05:36 -0500

    wip configure app envars

--- fullstack-app/vite.config.js
+++ fullstack-app/vite.config.js
@@ -1,18 +1,30 @@
 // frontend/vite.config.js
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-import config from '#src/config.js';
+// import config from '#src/config.js';
 
-export default defineConfig({
-  // add env to process for react
-  define: {
-    'process.env': {}
-  },
-  server: {
-    host: '0.0.0.0',
-    port: config.port, // Ensure this is consistent with your Docker Compose setup
-    strictPort: true // Ensures Vite will only start if the port is available
-  },
-  plugins: [react()]
+const { process } = globalThis;
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const define = {
+    'process.env.VERSION': JSON.stringify(env.VITE_VERSION),
+    // todo: conditionally set these based on the environment
+    'process.env.FULLSTACK_API_URL': JSON.stringify(env.VITE_FULLSTACK_API_URL),
+    'process.env.FULLSTACK_APP_URL': JSON.stringify(env.VITE_FULLSTACK_APP_URL)
+  };
+
+  return {
+    // add env to process for react
+    define,
+    server: {
+      host: '0.0.0.0',
+      // todo: revisit this
+      port: env.VITE_VERSION === 'development' ? 3000 : 80, // Ensure this is consistent with your Docker Compose setup
+      strictPort: true // Ensures Vite will only start if the port is available
+    },
+    plugins: [react()]
+  };
 });
